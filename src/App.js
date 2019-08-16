7cebfd5d0bffe906bca6a405245da9e69d960ee4
Suman <tht.suman@gmail.com>
2019-08-16 20:22:57 +0530

    add react-router-dom and start building shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './App.css'
 import Homepage from './pages/homepage/homepage.components'
 
+import ShopPage from './pages/shop/shop.components';
+
 const App = () => {
     return (
         <div>
-            <Homepage />
+            <Router>
+                <Route exact path='/' component={Homepage} />
+                <Route path='/shop' component={ShopPage} />
+            </Router>
         </div>
     )
 }
