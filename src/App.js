a332c62519cb24e91b28a57bdb8321716fd5c849
Suman <tht.suman@gmail.com>
2019-09-01 21:14:14 +0530

    include signup page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ShopPage from './pages/shop/shop.components';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -19,10 +19,22 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({ currentUser: user });
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async (authUser) => {
+            if (authUser) {
+                const userRef = await createUserProfileDocument(authUser);
 
-            console.log(user);
+                userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    });
+                });
+            }
+            else {
+                this.setState({ currentUser: null })
+            }
         });
     }
 
