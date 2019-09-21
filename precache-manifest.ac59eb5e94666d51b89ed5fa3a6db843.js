5353e0dae0101648ade8cd49191d622b8f58641f
Suman <tht.suman@gmail.com>
2019-09-21 20:13:01 +0530

    Updates

---
+++ precache-manifest.ac59eb5e94666d51b89ed5fa3a6db843.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2ae0d1151104e910ef906732f453ce",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "17c0e9cca3129d8ff999",
+    "url": "/crwn-clothing/static/css/main.2db78130.chunk.css"
+  },
+  {
+    "revision": "16e8c845aaf3f10015c4",
+    "url": "/crwn-clothing/static/js/2.6727ec44.chunk.js"
+  },
+  {
+    "revision": "17c0e9cca3129d8ff999",
+    "url": "/crwn-clothing/static/js/main.560374d6.chunk.js"
+  },
+  {
+    "revision": "e88d177d5d18f16bee32",
+    "url": "/crwn-clothing/static/js/runtime~main.675f06d2.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
