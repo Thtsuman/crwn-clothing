88390925b25a2e92fc16ebe56ef2627b7794b8b5
Suman <tht.suman@gmail.com>
2019-10-02 21:32:44 +0530

    add animation

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
+
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
@@ -8,34 +10,39 @@ import CheckoutItem from '../../components/checkout-item/checkout-item.component
 import './checkout.style.scss';
 
 const CheckoutPage = ({ cartItems, total }) => (
-    <div className="checkout-page">
-        <div className="checkout-header">
-            <div className="header-block">
-                <span>Products</span>
-            </div>
-            <div className="header-block">
-                <span>Description</span>
-            </div>
-            <div className="header-block">
-                <span>Quantity</span>
-            </div>
-            <div className="header-block">
-                <span>Price</span>
-            </div>
-            <div className="header-block">
-                <span>Remove</span>
-            </div>
-        </div>
-        {
-            cartItems.map(cartItem => (
-                <CheckoutItem cartItem={cartItem} />
-            ))
-        }
+    <Fade bottom opposite>
+        <div className="checkout-page">
 
-        <div className="total">
-            <span>TOTAL: ${total}</span>
+            <div className="checkout-header">
+                <div className="header-block">
+                    <span>Products</span>
+                </div>
+                <div className="header-block">
+                    <span>Description</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
+            </div>
+            {
+                cartItems.map(cartItem => (
+                    <CheckoutItem key={cartItems.id} cartItem={cartItem} />
+                ))
+            }
+
+
+
+            <div className="total">
+                <span>TOTAL: ${total}</span>
+            </div>
         </div>
-    </div>
+    </Fade>
 )
 
 const mapStateToProps = () => createStructuredSelector({
