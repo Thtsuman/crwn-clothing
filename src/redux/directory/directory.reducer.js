bd7db55f26e37cc837415f409563229ab945e75a
Suman <tht.suman@gmail.com>
2019-10-18 20:18:04 +0530

    remove logger from production build

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -8,7 +8,7 @@ const INITIAL_STATE = {
         },
         {
             title: 'JACKETS',
-            imageUrl: 'https://images.unsplash.com/photo-1509539662397-116cb90542f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+            imageUrl: 'https://images.unsplash.com/photo-1509539662397-116cb90542f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
             id: 2,
             linkUrl: 'shop/jackets'
         },
@@ -20,14 +20,14 @@ const INITIAL_STATE = {
         },
         {
             title: 'WOMENS',
-            imageUrl: 'https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+            imageUrl: 'https://images.unsplash.com/photo-1499111544217-9590382658d3?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
             size: 'large',
             id: 4,
             linkUrl: 'shop/womens'
         },
         {
             title: 'MENS',
-            imageUrl: 'https://images.unsplash.com/photo-1543322748-33df6d3db806?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
+            imageUrl: 'https://images.unsplash.com/photo-1552085473-429730a886d1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=564&q=80',
             size: 'large',
             id: 5,
             linkUrl: 'shop/mens'
